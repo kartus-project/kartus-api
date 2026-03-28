04fba3e0932a48f3fdf2695bc44ed8c90800c051
jes0131 <wkddmstn0131@gmail.com>
2026-03-28 11:11:23 +0900

    fix: 테스트용 api 삭제

--- src/main/java/com/kartus/api/domain/health/controller/HealthController.java
+++ src/main/java/com/kartus/api/domain/health/controller/HealthController.java
@@ -1,7 +1,5 @@
 package com.kartus.api.domain.health.controller;
 
-import com.kartus.api.global.security.UserPrincipal;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +10,5 @@ public class HealthController {
     @GetMapping
     public String health() {
         return "Server is running";
-    }
-
-    @GetMapping("test")
-    public Long test(@AuthenticationPrincipal UserPrincipal userPrincipal) {
-        return userPrincipal.getUserId();
     }
 }
