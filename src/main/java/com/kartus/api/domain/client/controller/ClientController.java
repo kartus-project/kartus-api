150248b4d94e68fe1762b701623222f37fc29e4f
jes0131 <162442223+jes0131@users.noreply.github.com>
2026-08-19 17:26:30 +0900

    feat: 클라이언트 다운로드 API 추가
    - GET /api/client/download?platform={platform}
    - 요청한 플랫폼의 최신 버전 실행 파일을 스트리밍

---
+++ src/main/java/com/kartus/api/domain/client/controller/ClientController.java
@@ -0,0 +1,34 @@
+package com.kartus.api.domain.client.controller;
+
+import com.kartus.api.domain.client.service.ClientDownloadService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/client")
+@RequiredArgsConstructor
+public class ClientController {
+    private final ClientDownloadService clientDownloadService;
+
+    @GetMapping("download")
+    public ResponseEntity<Resource> download(@RequestParam String platform) {
+        ClientDownloadService.ClientDownload download = clientDownloadService.resolve(platform);
+
+        ContentDisposition contentDisposition = ContentDisposition.attachment()
+                .filename(download.fileName())
+                .build();
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition.toString())
+                .body(download.resource());
+    }
+}
